9c76788b5128ade1dfcb460cfe62a961ba77d508
Jimmy Qiu <jiamingqiu951@gmail.com>
2018-01-04 00:43:00 -0800

    Initial setup of contact page, forms, and routes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
 from app import app
-from flask import render_template
+from flask import Flask, render_template, flash, redirect
+from app.forms import ContactForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home') 
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        return 'Form posted.'
+    return render_template('contact.html', form=form)
 
