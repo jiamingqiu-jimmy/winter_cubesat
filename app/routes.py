a33da89ae5b117f677e71cf8ff8dda04eddf2e2e
Jimmy Qiu <jiamingqiu951@gmail.com>
2018-01-04 01:32:32 -0800

    minor changes

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template, flash, redirect
 from app.forms import ContactForm
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('index.html', title='Home') 
+    return render_template('home.html', title='Home')
+
+@app.route('/services')
+def services():
+    return render_template('services.html', title='Services')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
